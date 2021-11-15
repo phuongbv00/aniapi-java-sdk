36e9d6844c2df854498d17d358e12d1cac867845
Phuong Bui <buiphuongaptutc@gmail.com>
2021-11-15 11:44:24 +0700

    test

--- src/test/java/io/github/censodev/sdk/aniapi/v1/AniApiClientTest.java
+++ src/test/java/io/github/censodev/sdk/aniapi/v1/AniApiClientTest.java
@@ -1,14 +1,15 @@
 package io.github.censodev.sdk.aniapi.v1;
 
+import io.github.censodev.sdk.aniapi.v1.domains.Anime;
 import io.github.censodev.sdk.aniapi.v1.domains.Pagination;
 import io.github.censodev.sdk.aniapi.v1.domains.filters.AnimeFilter;
 import io.github.censodev.sdk.aniapi.v1.enums.AnimeFormatEnum;
 import io.github.censodev.sdk.aniapi.v1.enums.AnimeSeasonEnum;
 import io.github.censodev.sdk.aniapi.v1.enums.AnimeStatusEnum;
+import io.github.censodev.sdk.aniapi.v1.enums.SortDirectionEnum;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AniApiClientTest {
     AniApiClient client = new AniApiClient();
@@ -25,11 +26,21 @@ class AniApiClientTest {
                 .year(1998)
                 .season(AnimeSeasonEnum.SPRING)
                 .genres(new String[]{"Action", "Guns", "Military"})
+                .sort("titles.en", SortDirectionEnum.DESCENDING)
                 .build();
         var res = client.getAnimeList(filter);
         assertNotNull(res);
         assertNotNull(res.getData());
         assertTrue(res.getData() instanceof Pagination);
         System.out.println(res.getData().getDocuments());
+    }
+
+    @Test
+    void testAnime() {
+        var res = client.getAnime(1L);
+        assertNotNull(res);
+        assertNotNull(res.getData());
+        assertTrue(res.getData() instanceof Anime);
+        System.out.println(res.getData());
     }
 }
